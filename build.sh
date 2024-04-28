339ed09f7b2868b50e6eb51e529a0ee117eaffae
Naoya Akimoto <akitkat.cook.dx@gmail.com>
2024-04-28 10:07:58 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 
 mkdir -p static/data
 
-# hotels.json
-wget "${JSON_URL_HOTELS}" -O 'hotels.json' &&
-mv -v hotels.json static/data/ &&
-
 # golfCourses.json
 wget "${JSON_URL_GOLF_COURSE}" -O 'golfCourses.json' &&
 mv -v golfCourses.json static/data/ &&
